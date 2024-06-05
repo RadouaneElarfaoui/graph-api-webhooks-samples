4988e6696c6aa4c7645519a060787afa1d31f887
alfred <100863418+RadouaneElarfaoui@users.noreply.github.com>
2024-06-05 20:29:22 +0100

    Update changeHandler.js

--- heroku/changeHandler.js
+++ heroku/changeHandler.js
@@ -7,17 +7,17 @@
 function handleChange(change) {
   console.log('Handling change:', JSON.stringify(change, null, 2));
     if (change.value && change.value.verb === "edited") { // Vérifie si le changement concerne une édition
-    // Extrait les informations pertinentes du changement
-    const postId = change.value.post_id;
-    const message = change.value.message;
-
-    // Affiche le message et l'ID du post
-    console.log(`Message: ${message}`);
-    console.log(`Post ID: ${postId}`);
-    
-    // Affiche un message indiquant que le post a été modifié
-    //console.log(`Le post avec l'ID ${postId} a été modifié.`);
-  }
+      // Extrait les informations pertinentes du changement
+      const postId = change.value.post_id;
+      const message = change.value.message;
+  
+      // Affiche le message et l'ID du post
+      console.log(`Message: ${message}`);
+      console.log(`Post ID: ${postId}`);
+      
+      // Affiche un message indiquant que le post a été modifié
+      //console.log(`Le post avec l'ID ${postId} a été modifié.`);
+    }
   
  
   // Ajoutez ici plus de logique de traitement selon vos besoins
