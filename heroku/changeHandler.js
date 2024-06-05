6709fb531e8dc2cfe2bfff4b13a37599324e5b4b
alfred <100863418+RadouaneElarfaoui@users.noreply.github.com>
2024-06-05 20:53:26 +0100

    Update changeHandler.js

--- heroku/changeHandler.js
+++ heroku/changeHandler.js
@@ -1,26 +1,41 @@
-// changeHandler.js
+const axios = require('axios');
 
 /**
  * Fonction pour traiter les changements reçus dans les webhooks
  * @param {Object} change - L'objet de changement à traiter
  */
-function handleChange(change) {
+async function handleChange(change) {
   console.log('Handling change:', JSON.stringify(change, null, 2));
-    /*if (change.value && change.value.verb === "edited") { // Vérifie si le changement concerne une édition
-      // Extrait les informations pertinentes du changement
-      const postId = change.value.post_id;
-      const message = change.value.message;
   
-      // Affiche le message et l'ID du post
-      console.log(`Message: ${message}`);
-      console.log(`Post ID: ${postId}`);
-      
-      // Affiche un message indiquant que le post a été modifié
-      //console.log(`Le post avec l'ID ${postId} a été modifié.`);
-    }*/
-  
- 
-  // Ajoutez ici plus de logique de traitement selon vos besoins
+  if (!change.value) {
+    console.log('No value in change object');
+    return;
+  }
+
+  if (change.value.verb === "edited") {
+    const postId = change.value.post_id;
+    const message = change.value.message;
+
+    console.log(`Message: ${message}`);
+    console.log(`Post ID: ${postId}`);
+
+    console.log(`Le post avec l'ID ${postId} a été modifié.`);
+
+    try {
+      const accessToken = process.env.PAGE_ACCESS_TOKEN; // Assurez-vous que votre access token est défini dans les variables d'environnement
+      const newMessage = "Le post a été modifié automatiquement.";
+      const response = await axios.post(
+        `https://graph.facebook.com/v20.0/${postId}`,
+        { message: newMessage },
+        { params: { access_token: accessToken } }
+      );
+      console.log(`Post modifié avec succès : ${response.data}`);
+    } catch (error) {
+      console.error(`Erreur lors de la modification du post : ${error.message}`);
+    }
+  } else {
+    console.log('Le changement reçu ne concerne pas une édition de post.');
+  }
 }
 
 module.exports = handleChange;
